eee99e89409239ff2cd336df828ac4b7ffd5559c
Anna Ivanova <kalter@deadsoftware.ru>
2015-07-29 16:00:08 +0500

    New exception: TemplateException

---
+++ src/com/temporaryteam/noticeditor/lngs/TemplateException.java
@@ -0,0 +1,11 @@
+package com.temporaryteam.noticeditor.lngs;
+
+/**
+ * @author kalterfive
+ */
+public class TemplateException extends Exception {
+
+	public TemplateException(String message) {
+		super(message);
+	}
+}
