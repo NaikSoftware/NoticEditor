39a2a54708c58bf4edc083a7764bc777da594853
Naik <naiksav4enko@gmail.com>
2015-08-28 12:41:25 +0300

    Fix font smoothing in WebView (on linux)

--- src/com/temporaryteam/treenote/Main.java
+++ src/com/temporaryteam/treenote/Main.java
@@ -10,6 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        System.setProperty("prism.lcdtext", "false");
         primaryStage.setTitle("TreeNote");
         ResourceBundle res = ResourceBundle.getBundle("resources.translate.Language", Locale.getDefault());
         Context.init(res, primaryStage);
